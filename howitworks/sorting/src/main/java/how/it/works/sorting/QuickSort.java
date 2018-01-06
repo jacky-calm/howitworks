63e4e3aa1251fb61f343e5f7b39981222ae5f215
javasaloon <calm2016@yeah.net>
2018-01-06 09:49:49 +0800

    partition works

--- howitworks/sorting/src/main/java/how/it/works/sorting/QuickSort.java
+++ howitworks/sorting/src/main/java/how/it/works/sorting/QuickSort.java
@@ -4,6 +4,25 @@ public class QuickSort {
 
 
     public void sort(int[] seq) {
-        
+        partition(seq, 0, seq.length);
+    }
+
+    public void partition(int[] seq, int begin, int end) {
+        int pivot = seq[begin];
+        int i = begin;
+        for (int j = begin + 1; j < end; j++) {
+            if (seq[j] < pivot) {
+                i++;
+                if (j > i) {
+                    int tem = seq[i];
+                    seq[i] = seq[j];
+                    seq[j] = tem;
+                }
+
+            }
+        }
+        seq[begin] = seq[i];
+        seq[i] = pivot;
+
     }
 }
--- howitworks/sorting/src/test/java/how/it/works/sorting/QuickSortTest.java
+++ howitworks/sorting/src/test/java/how/it/works/sorting/QuickSortTest.java
@@ -9,25 +9,44 @@ import static org.junit.Assert.*;
 public class QuickSortTest {
 
     QuickSort quickSort = new QuickSort();
-    private int[] seq;
-    private int[] sorted;
 
     @Before
     public void setUp() throws Exception {
-        seq = new int[]{3, 1, 5, 22, 8, 5, 7};
-        sorted = new int[]{1, 3, 5, 5, 7, 8, 22};
+
     }
 
     @Test
     public void sort() {
+        int[] seq = new int[]{10, 1, 5, 22, 8, 15, 7};
+        int[] sorted = new int[]{1, 3, 5, 8, 10, 15, 22};
         quickSort.sort(seq);
 
+        printArray(seq);
+
+
+        compareArray(seq, sorted);
+    }
+
+    private void printArray(int[] seq) {
         for (int i = 0; i < seq.length; i++) {
-            assertEquals("should equal", seq[i], sorted[i]);
+            System.out.println(seq[i]);
         }
     }
 
-    @After
-    public void tearDown() throws Exception {
+    private void compareArray(int[] a1, int[] a2) {
+        for (int i = 0; i < a1.length; i++) {
+            assertEquals(a1[i], a2[i]);
+        }
+    }
+
+    @Test
+    public void partition() {
+        int[] seq = new int[]{10, 1, 5, 22, 8, 15, 7};
+        int[] expect = new int[]{7, 1, 5, 8, 10, 15, 22};
+        quickSort.partition(seq, 0, seq.length);
+
+        printArray(seq);
+
+        compareArray(seq, expect);
     }
 }
