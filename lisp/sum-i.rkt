8d21b2cd848a6655886bc950e90600436ccd498a
Cheng Jacky <youngok2004@163.com>
2018-01-14 22:53:55 +0800

    refine

--- lisp/sum-i.rkt
+++ lisp/sum-i.rkt
@@ -11,18 +11,20 @@
       (+ (sum-int-square from (- to 1)) (square to)) )
   )
 
-(define (itself x) x)
+(define (identidy x) x)
 (define (square x) (* x x))
+(define (1+ x) (+ x 1))
 
 
-(define (sum-func from to func)
+(define (sum term from next to)
   (if (> from to)
       0
-      (+ (sum-func from (- to 1) func) (func to)) )
+      (+ (term from)
+         (sum term (next from) next to) ) )
   )
 
 (sum-int 1 3)
 (sum-int-square 1 3)
 
-(sum-func 1 3 itself)
-(sum-func 1 3 square)
+(sum identidy 1 1+ 3 )
+(sum square 1 1+ 3 )
